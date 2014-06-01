e2289db7a1796421626b27ef607423872f42885b
Nick Smith <nick.smith@torchbox.com>
2014-06-01 23:06:26 +0100

    Add English UK dvorak windows/linux layouts

--- content/keyboards/keyboards.js
+++ content/keyboards/keyboards.js
@@ -13,7 +13,9 @@ var sitedata_keyboards = {
         mac: "english-us-dvorak_mac.html",
     },
     "English UK Dvorak": {
+        windows: "english-uk-dvorak.html",
         mac: "english-uk-dvorak_mac.html",
+        linux: "english-uk-dvorak.html",
     },
     "Danish": {
         windows: "danish.html",
