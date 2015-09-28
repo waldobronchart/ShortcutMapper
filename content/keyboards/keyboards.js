2fa1e63f89af82f3751bf829777e71a079dc8abd
mdtrooper <tres.14159@gmail.com>
2015-09-28 04:46:28 +0200

    Added the first version of spanish keyboard layout

--- content/keyboards/keyboards.js
+++ content/keyboards/keyboards.js
@@ -27,5 +27,9 @@ var sitedata_keyboards = {
     "German": {
         windows: "german.html",
         linux: "german.html",
-    }
+    },
+	"Español": {
+		windows:	"spanish.html"
+		linux:		"spanish.html"
+	}
 };
